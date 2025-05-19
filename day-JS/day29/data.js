cc300a5c8103339f6e103022d1ccdfff2f5df21e
tucd2005 <tucdph51409@gmail.com>
2025-05-19 09:31:27 +0700

    clean main: remove code, keep folders only

--- day-JS/day29/data.js
+++
@@ -1,8 +0,0 @@
-const data = {
-    name : "Hoang",
-    age : 33,
-}
-export const myStudents = ["quynh anh", "huan"]
-console.log(myStudents);
-
-console.log(myInfor);
--- day-JS/day29/products.js
+++
@@ -1,4 +0,0 @@
-import myStudents , {myInfor} from "./data.js"
-console.log(myStudents([,,,]));
-console.log(myInfor);
-
